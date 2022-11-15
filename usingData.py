475db9d6099dfb8c7695a5acf2e937d37e4af95d
aajmorgan <ajmsd6@gmail.com>
2022-11-14 21:06:30 -0500

    preprocessed data

---
+++ usingData.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+labels = np.load('labels.npy') # 19436 labels
+data = np.load('data.npy')     # 19436 preprocessed images 
+                               # of x,y,z at 21 landmarks a hand
+
