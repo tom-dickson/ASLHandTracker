0b0530c2dfff22c5ead9263f347e56f13be1b292
aajmorgan <ajmsd6@gmail.com>
2022-11-14 21:20:52 -0500

    what

--- usingData.py
+++ usingData.py
@@ -4,3 +4,4 @@ labels = np.load('labels.npy') # 19436 labels
 data = np.load('data.npy')     # 19436 preprocessed images 
                                # of x,y,z at 21 landmarks a hand
 
+print(data)
